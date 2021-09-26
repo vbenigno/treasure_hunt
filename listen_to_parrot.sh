00b4667cd4918346920f3bacf4e96d62de18d920
Robin Engler <robin.engler@sib.swiss>
2021-09-27 00:53:33 +0200

    Make the parrot more cooperative

--- listen_to_parrot.sh
+++ listen_to_parrot.sh
@@ -5,8 +5,15 @@ source .script/quest_functions.sh
 parrot_say "You're here for John Longsilver's treasure?"
 sleep 1
 
-# Parrot is not being helpful...
-for i in $( seq 1 10); do
-    echo "I don't remember anything..."
-    sleep 2
-done
+# Parrot remembers stuff...
+echo "Ah I miss John... such a good pirate..."
+sleep 1
+echo "The treasure... yes, I was with John when he was hiding it!"
+sleep 1
+
+echo "At that time he often spent time in the following 3 locations:"
+echo " * key_1: Port-Royal, John Longsilver's ship is still docked there, visit the captain's cabin."
+echo " * key_2: Blackbird's castle, in the west tower of the castle."
+echo " * key_3: Oyster bay, the old lighthouse, that was a pirate meeting place."
+echo " "
+sleep 2
